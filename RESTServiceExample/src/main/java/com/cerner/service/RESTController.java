4c9944f167dae8dd9c234aa9f879ee6354d6ad6a
Ryan Provencher <ryan.provencher@cerner.com>
2018-10-15 17:14:19 -0400

    adding template change

--- RESTServiceExample/src/main/java/com/cerner/service/RESTController.java
+++ RESTServiceExample/src/main/java/com/cerner/service/RESTController.java
@@ -2,13 +2,15 @@ package com.cerner.service;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RESTController {
 
-    private static final String template = "Hello, %s!";
+    private String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
@@ -16,4 +18,17 @@ public class RESTController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+    @RequestMapping(value="/template", method=RequestMethod.PUT)
+    public String setTemplate(@RequestBody ServiceTemplate srvTemplate) {
+    	template = srvTemplate.getStr();
+        return "Done";
+    }
+    
+    @RequestMapping(value="/template", method=RequestMethod.GET)
+    public String setTemplateWithParam(@RequestParam(value="template") String srvTemplate) {
+    	template = srvTemplate;
+        return "Done";
+    }
+    
 }
---
+++ RESTServiceExample/src/main/java/com/cerner/service/ServiceTemplate.java
@@ -0,0 +1,13 @@
+package com.cerner.service;
+
+public class ServiceTemplate {
+	private String str;
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+}
